5de5bb2f2fbc85ed6f0c7c5a7dbb83e753ec92c8
woodwolfzhu <woodwolfzhu@gmail.com>
2018-07-24 20:45:32 +0800

    对关于文件的操作sys.argv有详细的说明

--- sys.argv_说明.py
+++ sys.argv_说明.py
@@ -3,3 +3,25 @@
 import sys
 a = sys.argv
 print(a)
+
+#书上范例程序
+# _*_ coding: utf-8 _*_
+#程序 8-4.py (Python 3 version)
+
+import sys
+
+if len(sys.argv)<2:
+    print("使用方法：python 8-4.py 成绩文件")
+    exit(1)
+
+no=1
+scores=dict()
+while True:
+    score = int(input('请输入第{}号的成绩:(-1结束)'.format(no)))
+    if score == -1: break;
+    scores[no] = score
+    no += 1
+
+with open(sys.argv[1],'w') as fp:         #也就是说不需要用变量来保存sys.argv的值
+    fp.write(str(scores))
+print("{}已存储完毕".format(sys.argv[1]))
