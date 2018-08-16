d4bded5729ffee40b34bae560e829e0baa7e9f87
woodwolfzhu <woodwolfzhu@gmail.com>
2018-08-16 14:25:19 +0800

    默认

--- 测试.py
+++ 测试.py
@@ -1,13 +1,23 @@
-def test(a,b):
-    """
+class Person:
 
-    :param a: vvsd
-    :param b: sdf
-    """
-    print('%d' % int(a+b))
+    def __init__(self,new_name,new_weight):
+        self.name = new_name
+        self.weight = int(new_weight)
 
-test(23,43)
+    def eat(self,num):
+        self.weight += int(num)
+        print("%s's weight had gain %d kg!" % (self.name,int(num)))
+
+    def run(self,num):
+        self.weight -= 0.5 * int(num)
+        print("%s's weight had decreased %d kg!" % (self.name,0.5 * int(num)))
 
 
-list1 = []
 
+xiaoming = Person("huying", 45)
+print("my name is {},my weight is {} kg".format(xiaoming.name, xiaoming.weight))
+a = input("how many times I had ran?")
+b = input("how many times I had ate?")
+xiaoming.run(a)
+xiaoming.eat(b)
+print("My weight is {} kg today!".format(xiaoming.weight))
